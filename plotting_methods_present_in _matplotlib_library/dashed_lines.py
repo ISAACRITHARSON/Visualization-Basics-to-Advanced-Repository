01316f7449ee233fa2f96915a581b2bc12a63a76
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-06-20 09:44:06 +0530

    Create dashed_lines.py

---
+++ plotting_methods_present_in _matplotlib_library/dashed_lines.py
@@ -0,0 +1,7 @@
+x=[2,2,9,9]
+y=[2,5,5,2]
+plt.title('question 3')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.plot(x,y,color='green',linestyle='dashed',linewidth=3,marker='o',marke
+rfacecolor='blue',markersize=12)
