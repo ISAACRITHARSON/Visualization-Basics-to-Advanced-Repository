453d6091e6d6d7a4096b66e1c69acc87e81b9a15
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-06-20 09:26:08 +0530

    Create double_line.py

---
+++ plotting_methods_present_in _matplotlib_library/double_line.py
@@ -0,0 +1,12 @@
+x1=[2,5,9]
+y1=[2,5,7]
+plt.title('question 1')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.plot(x1,y1)
+x2=[6,5,5]
+y2=[9,5,5]
+plt.title('question 1')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.plot(x2,y2)
